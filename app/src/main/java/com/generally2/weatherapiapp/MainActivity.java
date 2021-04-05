3ea9c50ca12e811e58c22933bca4cb6f4955fb5b
Generally2 <dailyspecial_719@yahoo.com>
2021-04-04 19:20:20 -0600

    added json data fetching functionality, app can get city id from metaweather.com/api

--- app/src/main/java/com/generally2/weatherapiapp/MainActivity.java
+++ app/src/main/java/com/generally2/weatherapiapp/MainActivity.java
@@ -3,12 +3,107 @@ package com.generally2.weatherapiapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button btn_city, btn_getWeatherById, btn_getWeatherByName;
+    EditText et_dataInput;
+    ListView lv_weatherReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_city = findViewById(R.id.btn_getCityId);
+        btn_getWeatherById = findViewById(R.id.btn_getWeatherByCityId);
+        btn_getWeatherByName = findViewById(R.id.btn_getWeatherByCityName);
+        et_dataInput = findViewById(R.id.et_dataInput);
+        lv_weatherReport = findViewById(R.id.lv_weatherReport);
+
+        btn_getWeatherById.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You Clicked me 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You typed " + et_dataInput.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_city.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url ="https://www.metaweather.com/api/location/search/?query=" + et_dataInput.getText().toString();
+
+                JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        String cityId = "";
+                        try {
+                            JSONObject cityInfo = response.getJSONObject(0);
+                            cityId  = cityInfo.getString("woeid");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        Toast.makeText(MainActivity.this, "City Id " + cityId, Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                  // Request a string response from the provided URL.
+                /*StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Error Occurred.", Toast.LENGTH_SHORT).show();
+                    }
+                });*/
+
+                  // Add the request to the RequestQueue.
+                queue.add(request);
+                //Toast.makeText(MainActivity.this, "You Clicked me 1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+
     }
 }
